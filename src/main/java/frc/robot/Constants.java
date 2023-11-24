95aa715f473605c844048540c1da97cff41154c4
BreadSpeed1 <aiden@voth.name>
2023-11-24 13:41:38 -0500

    swerve drive n whatevs

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,5 +1,88 @@
 package frc.robot;
 
-public class Constants {
-    
+import com.ctre.phoenixpro.configs.CurrentLimitsConfigs;
+import edu.wpi.first.math.geometry.Translation2d;
+import frc.robot.ShamLib.PIDGains;
+import frc.robot.ShamLib.motors.pro.PIDSVGains;
+import frc.robot.ShamLib.swerve.ModuleInfo;
+
+import java.util.function.UnaryOperator;
+
+public final class Constants {
+    public static final CurrentLimitsConfigs CURRENT_LIMITS_CONFIGS = new CurrentLimitsConfigs();
+
+    public static final class Controller {
+        public static final double DEADBAND = 0.025;
+        public static final UnaryOperator<Double> CONVERTER = (input) -> (Math.copySign(input * input, input));
+    }
+
+    public static final class Drivetrain {
+        public static final String GYRO_CAN_BUS = "";
+        public static final int PIGEON_CAN_ID = 0;
+
+        public static final double MAX_CHASSIS_SPEED = 0.0;
+        public static final double MAX_CHASSIS_ACCELERATION = 0.0;
+
+        public static final PIDGains TELEOP_THETA_GAINS = new PIDGains(0, 0, 0);
+        public static final PIDGains AUTO_THETA_GAINS = new PIDGains(0, 0, 0);
+        public static final PIDGains TRANSLATION_GAINS = new PIDGains(0, 0, 0);
+
+        public static final class Modules {
+            public static final String CAN_BUS = "";
+
+            public static final double MAX_TURN_SPEED = 0.0;
+            public static final double MAX_TURN_ACCELERATION = 0.0;
+
+            public static final PIDSVGains DRIVE_GAINS = new PIDSVGains(0, 0, 0, 0, 0);
+            public static final PIDSVGains TURN_GAINS = new PIDSVGains(0, 0, 0, 0, 0);
+
+            public static final ModuleInfo MODULE_1 = new ModuleInfo(
+                    0,
+                    0,
+                    0,
+                    0.0,
+                    new Translation2d(0.0, 0.0),
+                    0.0,
+                    0.0,
+                    false,
+                    false
+            );
+
+            public static final ModuleInfo MODULE_2 = new ModuleInfo(
+                    0,
+                    0,
+                    0,
+                    0.0,
+                    new Translation2d(0.0, 0.0),
+                    0.0,
+                    0.0,
+                    false,
+                    false
+            );
+
+            public static final ModuleInfo MODULE_3 = new ModuleInfo(
+                    0,
+                    0,
+                    0,
+                    0.0,
+                    new Translation2d(0.0, 0.0),
+                    0.0,
+                    0.0,
+                    false,
+                    false
+            );
+
+            public static final ModuleInfo MODULE_4 = new ModuleInfo(
+                    0,
+                    0,
+                    0,
+                    0.0,
+                    new Translation2d(0.0, 0.0),
+                    0.0,
+                    0.0,
+                    false,
+                    false
+            );
+        }
+    }
 }
--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -1,18 +1,71 @@
 package frc.robot.subsystems;
 
+import frc.robot.Constants;
 import frc.robot.ShamLib.SMF.StateMachine;
+import frc.robot.ShamLib.swerve.DriveCommand;
+import frc.robot.ShamLib.swerve.SwerveDrive;
+
+import java.util.function.Supplier;
+
+import static frc.robot.Constants.Drivetrain.*;
 
 public class Drivetrain extends StateMachine<Drivetrain.State> {
-    public Drivetrain() {
+    private SwerveDrive serveDrive;
+
+    private Supplier<Double> xSupplier;
+    private Supplier<Double> ySupplier;
+    private Supplier<Double> thetaSupplier;
+
+    public Drivetrain(Supplier<Double> xSupplier, Supplier<Double> ySupplier, Supplier<Double> thetaSupplier) {
         super("Drivetrain", State.Undetermined, State.class);
+
+        this.xSupplier = xSupplier;
+        this.ySupplier = ySupplier;
+        this.thetaSupplier = thetaSupplier;
+
+        this.serveDrive = new SwerveDrive(
+                PIGEON_CAN_ID,
+                Modules.DRIVE_GAINS,
+                Modules.TURN_GAINS,
+                MAX_CHASSIS_SPEED,
+                MAX_CHASSIS_ACCELERATION,
+                Modules.MAX_TURN_SPEED,
+                Modules.MAX_TURN_ACCELERATION,
+                TELEOP_THETA_GAINS,
+                AUTO_THETA_GAINS,
+                TRANSLATION_GAINS,
+                false, //Set to true for extra telemetry
+                Modules.CAN_BUS,
+                GYRO_CAN_BUS,
+                Constants.CURRENT_LIMITS_CONFIGS,
+                Modules.MODULE_1,
+                Modules.MODULE_2,
+                Modules.MODULE_3,
+                Modules.MODULE_4
+        );
+    }
+
+    private void registerStates() {
+        registerStateCommand(State.TeleopFieldOriented, new DriveCommand(
+                serveDrive,
+                xSupplier,
+                ySupplier,
+                thetaSupplier,
+                Constants.Controller.DEADBAND,
+                Constants.Controller.CONVERTER,
+
+        ));
     }
 
     @Override
     protected void determineSelf() {
-
+        setState(State.Idle);
     }
 
     public enum State {
-        Undetermined
+        Undetermined,
+        Idle,
+        TeleopFieldOriented,
+        TeleopBotOriented
     }
 }
