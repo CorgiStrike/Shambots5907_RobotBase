bd38406ceaa3ba118a95f746dea4db484d9470f7
Jacob Barta <jacob.barta25@gmail.com>
2023-11-29 13:22:36 -0500

    delete swerve specific comments and add several todos for the constants file

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -24,13 +24,15 @@ public final class Constants {
     }
 
     public static final class Drivetrain {
+        //TODO: SET GYRO CAN INFO
         public static final String GYRO_CAN_BUS = "";
         public static final int PIGEON_CAN_ID = 1;
 
+        //TODO: SET TRACK WIDTH AND WHEEL_BASE
         // Distance between centers of right and left wheels on robot in meters
-        public static final double TRACK_WIDTH = Units.inchesToMeters(24);
+        public static final double TRACK_WIDTH = Units.inchesToMeters(0);
         // Distance between front and back wheels on robot in meters
-        public static final double WHEEL_BASE = Units.inchesToMeters(24);
+        public static final double WHEEL_BASE = Units.inchesToMeters(0);
 
         public static final double rotationRadius = Math.sqrt(Math.pow(TRACK_WIDTH / 2.0, 2) + Math.pow(WHEEL_BASE / 2.0, 2)) * 2 * Math.PI;
 
@@ -54,6 +56,7 @@ public final class Constants {
                 new SwerveModuleState(0, Rotation2d.fromDegrees(-45))
         };
 
+        //TODO: TUNE AUTO PID GAINS
         public static final PIDGains AUTO_THETA_GAINS = new PIDGains(0, 0, 0);
         public static final PIDGains AUTO_TRANSLATION_GAINS = new PIDGains(0, 0, 0);
 
@@ -63,35 +66,42 @@ public final class Constants {
             public static final double WHEEL_X_OFFSET = TRACK_WIDTH/2;
             public static final double WHEEL_Y_OFFSET = WHEEL_BASE/2;
 
+            //These are values we used in 2023. They seem to work with no issues
             public static final double MAX_TURN_SPEED = 1000;
             public static final double MAX_TURN_ACCELERATION = 1000;
 
+            //TODO: Specify information about what type of modules you're running
             public static final SwerveModuleType MODULE_TYPE = SwerveModuleType.MK4;
             public static final SwerveModuleSpeedLevel SPEED_LEVEL = SwerveModuleSpeedLevel.L3;
 
+            //TODO: CALCULATE TURN AND DRIVE GAINS
+            //TODO: DOCUMENTATION FOR FALCON AND SWERVE TUNING PROCESS
             public static final PIDSVGains DRIVE_GAINS = new PIDSVGains(
-                0.25,
                 0,
                 0,
-                0.3,
-                0.1135
-                );
+                0,
+                0,
+                0
+            );
 
-                public static final PIDSVGains TURN_GAINS = new PIDSVGains(
-                10,
+            public static final PIDSVGains TURN_GAINS = new PIDSVGains(
                 0,
                 0,
-                0.3,
-                0.121057
-                );
+                0,
+                0,
+                0
+            );
+
+            //TODO: FILL IN MODULE CAN AND ENCODER OFFSET INFO
+            //TODO: You may have to change whether the drive motors are inverted or not. The existing inversions should work for a standard MK4
 
             //front left
             public static final ModuleInfo MODULE_1 = ModuleInfo.generateModuleInfo(
                 MODULE_TYPE, SPEED_LEVEL,
-                    1,
-                    2,
-                    1,
-                    -25.752,
+                    0,
+                    0,
+                    0,
+                    0,
                     new Translation2d(WHEEL_X_OFFSET, WHEEL_Y_OFFSET),
                     false
             );
@@ -99,10 +109,10 @@ public final class Constants {
             //back left
             public static final ModuleInfo MODULE_2 = ModuleInfo.generateModuleInfo(
                 MODULE_TYPE, SPEED_LEVEL,
-                    3,
-                    4,
-                    3,
-                    -165.1,
+                    0,
+                    0,
+                    0,
+                    0,
                     new Translation2d(-WHEEL_X_OFFSET, WHEEL_Y_OFFSET),
                     false
             );
@@ -110,10 +120,10 @@ public final class Constants {
             //back right
             public static final ModuleInfo MODULE_3 = ModuleInfo.generateModuleInfo(
                 MODULE_TYPE, SPEED_LEVEL,
-                    5,
-                    6,
-                    5,
-                    75.6,
+                    0,
+                    0,
+                    0,
+                    0,
                     new Translation2d(-WHEEL_X_OFFSET, -WHEEL_Y_OFFSET),
                     true
             );
@@ -121,10 +131,10 @@ public final class Constants {
             //front right
             public static final ModuleInfo MODULE_4 = ModuleInfo.generateModuleInfo(
                 MODULE_TYPE, SPEED_LEVEL,
-                    7,
-                    8,
-                    7,
-                    -84.9,
+                    0,
+                    0,
+                    0,
+                    0,
                     new Translation2d(WHEEL_X_OFFSET, -WHEEL_Y_OFFSET),
                     true
             );
